4c30b2f798abaa133c33d3e830e7b7ed6bad378d
Vidit Khare <viditkhare990@gmail.com>
2026-08-02 05:04:39 +0530

    Fix Gmail SMTP for production

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,16 @@
-import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  requireTLS: true,
 });
 
 export const sendVerificationEmail = async (email, otp) => {
