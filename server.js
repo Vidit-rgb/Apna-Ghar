6b807d4c6586f4e9be23b541bb8cebddf92da8c7
Vidit Khare <viditkhare990@gmail.com>
2026-08-02 16:28:59 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://apnaghar-frontend-nine.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
